2c16608ead6e53592ef5321c06f6be6d27c86ef0
Dmitry Mozzherin <dmozzherin@gmail.com>
2014-10-09 12:56:18 -0400

    small fix

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Dmitry Mozzherin"
 maintainer_email "dmozzherin@gmail.com"
 license          "MIT"
 description      "Installs/Configures eol-fixes"
-long_description File.read(File.join(__dir__, "README.md"))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.1"
 
 %w(apt yum).each do |cb|
